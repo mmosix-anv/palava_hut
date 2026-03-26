165e7b602621047065730b920f27b9157741256a
Moses Oyewole <moses@anvillive.com>
2026-03-26 19:32:07 +0000

    Update database credentials in Database.php to use specific user and password

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,8 +33,8 @@ class Database extends Config
 	public $default = [
 		'DSN'      => '',
 		'hostname' => 'localhost',
-		'username' => 'root',
-		'password' => '',
+		'username' => 'dash_phuser',
+		'password' => '^Ad#9.)[*%Ho',
 		'database' => 'ph_portal',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => 'ph_',
